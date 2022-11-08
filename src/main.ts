73ebf793e2c5e1d8fa68811d5aa7f3db20b97ee5
DzhumabaevaAliza <dzhumabaeva_a@auca.kg>
2022-11-08 16:48:43 +0600

    swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,36 @@
-import { NestFactory } from '@nestjs/core';
+import {
+  ClassSerializerInterceptor,
+  Logger,
+  ValidationPipe,
+} from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { NestFactory, Reflector } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule, { cors: true });
+  app.enableCors({
+    origin: '*',
+    credentials: true,
+  });
+
+  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
+
+  const config = new DocumentBuilder()
+    .addBearerAuth()
+    .setTitle('Google Classroom')
+    .setDescription('Google Classroom Documentation')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('classroom', app, document);
+
+  const configService = app.get(ConfigService);
+  const PORT = configService.get('PORT');
+
+  await app.listen(PORT || 5000, () => {
+    Logger.log(`Server started on PORT ${PORT}`);
+  });
 }
 bootstrap();
