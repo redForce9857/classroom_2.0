f3b5fb0408b0ce785b7b8d0bf6f846988e9ada0d
muratbek-kkebtarum <im.nurmatov@gmail.com>
2022-11-10 10:13:21 +0600

    migration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,27 +1,10 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
+import { dataSourceOptions } from '../db/data-source';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-      load: [() => process.env],
-    }),
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.POSTGRES_HOST,
-      port: Number(process.env.POSTGRES_PORT),
-      username: process.env.POSTGRES_USER,
-      password: process.env.POSTGRES_PASSWORD,
-      database: process.env.POSTGRES_DATABASE,
-      entities: [__dirname + '/../**/*.entity.js'],
-      synchronize: true,
-      namingStrategy: new SnakeNamingStrategy(),
-    }),
-  ],
-
+  imports: [ConfigModule.forRoot(), TypeOrmModule.forRoot(dataSourceOptions)],
   controllers: [],
   providers: [],
 })
