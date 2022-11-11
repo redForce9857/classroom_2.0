8336157304e21f1607c0619c683e33c6df38250f
DzhumabaevaAliza <dzhumabaeva_a@auca.kg>
2022-11-11 15:47:43 +0600

    google auth

---
+++ src/google-auth/utils/googleStrategy.ts
@@ -0,0 +1,29 @@
+import { Injectable, Inject } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { Profile, Strategy, VerifyCallback } from 'passport-google-oauth20';
+import { GoogleAuthService } from '../google-auth.service';
+@Injectable()
+export class GoogleAuthStrategy extends PassportStrategy(Strategy, 'google') {
+  constructor(
+    @Inject(GoogleAuthService) private readonly authService: GoogleAuthService,
+  ) {
+    super({
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+      callbackURL: 'http://localhost:3000/auth/google/redirect',
+      scope: ['profile', 'email'],
+    });
+  }
+  async validate(
+    accessToken: string,
+    refreshToken: string,
+    profile: Profile,
+    done: VerifyCallback,
+  ) {
+    const user = await this.authService.validateUser({
+      email: profile.emails[0].value,
+      displayName: profile.displayName,
+    });
+    done(null, user);
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,31 +1,69 @@
-import {
-  ClassSerializerInterceptor,
-  Logger,
-  ValidationPipe,
-} from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
-import { NestFactory, Reflector } from '@nestjs/core';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+// import {
+//   ClassSerializerInterceptor,
+//   Logger,
+//   ValidationPipe,
+// } from '@nestjs/common';
+// import { ConfigService } from '@nestjs/config';
+// import { NestFactory, Reflector } from '@nestjs/core';
+// import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+// import { AppModule } from './app.module';
+// import * as passport from 'passport';
+// async function bootstrap() {
+//   const app = await NestFactory.create(AppModule);
+//   // app.enableCors({
+//   //   origin: '*',
+//   //   credentials: true,
+//   // });
+//    app.setGlobalPrefix('api');
+
+//   app.useGlobalPipes(new ValidationPipe());
+//   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
+
+//   const config = new DocumentBuilder()
+//     .addBearerAuth()
+//     .setTitle('Google Classroom')
+//     .setDescription('Google Classroom Documentation')
+//     .build();
+//   const document = SwaggerModule.createDocument(app, config);
+//   SwaggerModule.setup('api', app, document);
+//   app.use(passport.initialize());
+
+//   const configService = app.get(ConfigService);
+//   const PORT = configService.get('PORT');
+
+//   await app.listen(PORT || 5000, () => {
+//     Logger.log(`Server started on PORT ${PORT}`);
+//   });
+// }
+// bootstrap();
+import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as passport from 'passport';
+import * as session from 'express-session';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+import { ConfigService } from '@nestjs/config';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
-  app.enableCors({
-    origin: '*',
-    credentials: true,
-  });
-
-  app.useGlobalPipes(new ValidationPipe());
-  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
-
+  const app = await NestFactory.create(AppModule);
+  app.use(session({
+    secret:'sessionSecret',
+    saveUninitialized:false,
+    cookie:{
+      maxAge:60000,
+    }
+  }))
+  app.use(passport.initialize());
+  app.use(passport.session());
   const config = new DocumentBuilder()
     .addBearerAuth()
     .setTitle('Google Classroom')
     .setDescription('Google Classroom Documentation')
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('classroom', app, document);
-
+  SwaggerModule.setup('api', app, document);
+  
   const configService = app.get(ConfigService);
   const PORT = configService.get('PORT');
 
