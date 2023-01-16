7123f735666a137e5d3c71d1a69eea03bfb93d7f
DenisLokhtin <denis.lokhtin@gmail.com>
2023-01-16 10:36:21 +0600

    delete stream and tasks-stream

--- src/tasks-stream/dto/update-tasks-stream.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from "@nestjs/swagger";
-import { CreateTasksStreamDto } from "./create-tasks-stream.dto";
-
-export class UpdateTasksStreamDto extends PartialType(CreateTasksStreamDto) {}
--- src/tasks-stream/tasks-stream.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from "@nestjs/common";
-import { TasksStreamService } from "./tasks-stream.service";
-import { TasksStreamController } from "./tasks-stream.controller";
-
-@Module({
-  controllers: [TasksStreamController],
-  providers: [TasksStreamService],
-})
-export class TasksStreamModule {}
