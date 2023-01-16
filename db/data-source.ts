dad27a8fc6f73a50e6a39bdcdc90608293e38a14
DenisLokhtin <denis.lokhtin@gmail.com>
2023-01-16 10:23:24 +0600

    add Number() to port

--- db/data-source.ts
+++ db/data-source.ts
@@ -1,11 +1,12 @@
 import { DataSource, DataSourceOptions } from "typeorm";
 import * as dotenv from "dotenv";
+
 dotenv.config();
 
 export const dataSourceOptions: DataSourceOptions = {
   type: "postgres",
   host: process.env.POSTGRES_HOST,
-  port: +process.env.POSTGRES_PORT,
+  port: Number(process.env.POSTGRES_PORT),
   username: process.env.POSTGRES_USER,
   password: process.env.POSTGRES_PASSWORD,
   database: process.env.POSTGRES_DATABASE,
